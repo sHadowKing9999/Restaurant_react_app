96ce935d40199d48e2dfed0d774799e837c1e0b4
ReetRoy <royreet52000@gmail.com>
2020-10-25 21:14:36 +0530

    react component part 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import {Navbar,NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent';
 function App() {
   return (
     <div className="App">
@@ -10,6 +9,7 @@ function App() {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu/>
     </div>
   );
 }
